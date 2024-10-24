60528e7d761876b310f7776f17bbe0df1f72f714
fangyefeng <1250890838@qq.com>
2024-10-24 09:04:08 +0800

    add opencv static library

---
+++ OpenCV430/x64/vc14/staticlib/OpenCVModules-release.cmake
@@ -0,0 +1,149 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "zlib" for configuration "Release"
+set_property(TARGET zlib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(zlib PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/zlib.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS zlib )
+list(APPEND _IMPORT_CHECK_FILES_FOR_zlib "${_IMPORT_PREFIX}/x64/vc14/staticlib/zlib.lib" )
+
+# Import target "libjpeg-turbo" for configuration "Release"
+set_property(TARGET libjpeg-turbo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libjpeg-turbo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/libjpeg-turbo.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libjpeg-turbo )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libjpeg-turbo "${_IMPORT_PREFIX}/x64/vc14/staticlib/libjpeg-turbo.lib" )
+
+# Import target "libtiff" for configuration "Release"
+set_property(TARGET libtiff APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libtiff PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/libtiff.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libtiff )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libtiff "${_IMPORT_PREFIX}/x64/vc14/staticlib/libtiff.lib" )
+
+# Import target "libwebp" for configuration "Release"
+set_property(TARGET libwebp APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libwebp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/libwebp.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libwebp )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libwebp "${_IMPORT_PREFIX}/x64/vc14/staticlib/libwebp.lib" )
+
+# Import target "libjasper" for configuration "Release"
+set_property(TARGET libjasper APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libjasper PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/libjasper.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libjasper )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libjasper "${_IMPORT_PREFIX}/x64/vc14/staticlib/libjasper.lib" )
+
+# Import target "libpng" for configuration "Release"
+set_property(TARGET libpng APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libpng PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/libpng.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libpng )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libpng "${_IMPORT_PREFIX}/x64/vc14/staticlib/libpng.lib" )
+
+# Import target "IlmImf" for configuration "Release"
+set_property(TARGET IlmImf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(IlmImf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/IlmImf.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS IlmImf )
+list(APPEND _IMPORT_CHECK_FILES_FOR_IlmImf "${_IMPORT_PREFIX}/x64/vc14/staticlib/IlmImf.lib" )
+
+# Import target "ippiw" for configuration "Release"
+set_property(TARGET ippiw APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ippiw PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/ippiw.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ippiw )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ippiw "${_IMPORT_PREFIX}/x64/vc14/staticlib/ippiw.lib" )
+
+# Import target "libprotobuf" for configuration "Release"
+set_property(TARGET libprotobuf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libprotobuf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/libprotobuf.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libprotobuf )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libprotobuf "${_IMPORT_PREFIX}/x64/vc14/staticlib/libprotobuf.lib" )
+
+# Import target "quirc" for configuration "Release"
+set_property(TARGET quirc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(quirc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/quirc.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS quirc )
+list(APPEND _IMPORT_CHECK_FILES_FOR_quirc "${_IMPORT_PREFIX}/x64/vc14/staticlib/quirc.lib" )
+
+# Import target "ittnotify" for configuration "Release"
+set_property(TARGET ittnotify APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ittnotify PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/ittnotify.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ittnotify )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ittnotify "${_IMPORT_PREFIX}/x64/vc14/staticlib/ittnotify.lib" )
+
+# Import target "ade" for configuration "Release"
+set_property(TARGET ade APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ade PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/ade.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ade )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ade "${_IMPORT_PREFIX}/x64/vc14/staticlib/ade.lib" )
+
+# Import target "opencv_world" for configuration "Release"
+set_property(TARGET opencv_world APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_world PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/opencv_world430.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_world )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_world "${_IMPORT_PREFIX}/x64/vc14/staticlib/opencv_world430.lib" )
+
+# Import target "opencv_img_hash" for configuration "Release"
+set_property(TARGET opencv_img_hash APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_img_hash PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/vc14/staticlib/opencv_img_hash430.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_img_hash )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_img_hash "${_IMPORT_PREFIX}/x64/vc14/staticlib/opencv_img_hash430.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
